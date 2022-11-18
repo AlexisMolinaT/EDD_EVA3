3b17e90c0d00eb91af4891b3d74d71aa5cc7e7d7
AlexisMolinaT <104458053+AlexisMolinaT@users.noreply.github.com>
2022-11-18 09:56:26 -0600

    Add files via upload

--- EVA_3_1_ORDENAMIENTOS/src/eva_3_1_ordenamientos/EVA_3_1_ORDENAMIENTOS.java
+++ EVA_3_1_ORDENAMIENTOS/src/eva_3_1_ordenamientos/EVA_3_1_ORDENAMIENTOS.java
@@ -1,8 +1,7 @@
 package eva_3_1_ordenamientos;
 
 /**
- *
- * @author Alexis M.
+ * @author moviles
  */
 public class EVA_3_1_ORDENAMIENTOS {
 
@@ -11,20 +10,29 @@ public class EVA_3_1_ORDENAMIENTOS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] arregloDatos = new int[10000];
+        int[] arregloDatos = new int[15];
         int[] arregloSel = new int[arregloDatos.length];
         System.out.println("ARREGLO ORIGINAL: ");
         llenar(arregloDatos);
-        //imprimir(arregloDatos);
-        System.out.println("SELECTION SORT: ");
-        copiar(arregloDatos, arregloSel);
-        //imprimir(arregloSel);
-        System.out.println("ARREGLO ORDENADO:");
+        imprimir(arregloDatos);
+
+        System.out.println("SELECCTION SORT: ");
+        copia(arregloDatos, arregloSel);
+        imprimir(arregloSel);
         double ini = System.nanoTime();
         selectionSort(arregloSel);
         double fin = System.nanoTime();
-        //imprimir(arregloSel);
-        System.out.println("Tiempo que le toma ordenar = " + (fin - ini));
+        imprimir(arregloSel);
+        System.out.println("Selection sort = " + (fin - ini));
+        
+        System.out.println("INSERTION   SORT: ");
+        copia(arregloDatos, arregloSel);
+        imprimir(arregloSel);
+        ini = System.nanoTime();
+        selectionSort(arregloSel);
+        fin = System.nanoTime();
+        imprimir(arregloSel);
+        System.out.println("Insertion sort = " + (fin - ini));
     }
 
     //LLENAR ARREGLO
@@ -39,45 +47,58 @@ public class EVA_3_1_ORDENAMIENTOS {
         for (int i = 0; i < arreglo.length; i++) {
             if (arreglo[i] < 10) {
                 System.out.print("[" + arreglo[i] + "]");
-            } else 
+            } else {
                 System.out.print("[" + arreglo[i] + "]");
-            
-
+            }
         }
         System.out.println("");
+
     }
 
-    public static void copiar(int[] arreglo, int[] copia) {
+    //COPIA ARREGLO
+    public static void copia(int[] arreglo, int[] copia) {
         for (int i = 0; i < arreglo.length; i++) {
             copia[i] = arreglo[i];
         }
     }
-    
-    //SELECTION SORT (n^2)
-    //COMPARACIONES
-    //INTERCAMBIOS (SWAP)
-    public static void selectionSort(int[] arreglo){
+
+    //SELECCTION SORT O(n^)
+    public static void selectionSort(int[] arreglo) {
         for (int i = 0; i < arreglo.length; i++) {
             int min = i;
-            //BUSCO EL ELEMENTO(POSICION) MAS PEQUEÑO
+            //BUSCO EL ELEMENTO (POSICION) MAS PEQUEñO
             for (int j = i + 1; j < arreglo.length; j++) {
-                //buscar el numero mas pequeño
-                if(arreglo[min] > arreglo[j])
+                //buscar el mas pequeño
+                if (arreglo[min] > arreglo[j]) {
                     min = j;
-                
+                }
             }
-            //SWAP INTERCAMBIAR
-             if(min != i){
-                 int temp = arreglo[i];
-                 arreglo[i] = arreglo[min];
-                 arreglo[min] = temp;
-             }
+            //SWAP INTERRCAMBIAR
+            if (min != i) {
+                int temp = arreglo[i];
+                arreglo[i] = arreglo[min];
+                arreglo[min] = temp;
+            }
         }
     }
-    
-    
-    
-    
-    
-    
+
+    //INSERTION SORT O(n^2)
+    public static void insertionSort(int[] arreglo) {
+        for (int i = 1; i < arreglo.length; i++) {
+            int temp = arreglo[i]; //es el valor que vamos a ordenar
+            int insP = i;
+            //EN QUE PUNTO DEBE DE QUEDAR TEMP
+            //ESA POSICION QUEDA ALMACENADA EN insP
+            for (int prev = i - 1; prev >= 0; prev--) { //comparar
+                if (arreglo[prev] > temp) {
+                    //swap (intercambio parcial)
+                    arreglo[insP] = arreglo[prev];
+                    insP--;
+                } else {
+                    break;
+                }
+            }
+            arreglo[insP] = temp;
+        }
+    }
 }
